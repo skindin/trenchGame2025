7dc52aa4a2a3400527ec30e5bd84f6bb6ca4a0b3
skindin <90371126+skindin@users.noreply.github.com>
2024-06-27 23:49:39 -0600

    actual bullet mesh instancing yayyy

--- Trench Game (V2)/Assets/material/bulletShader.shader
+++ Trench Game (V2)/Assets/material/bulletShader.shader
@@ -19,24 +19,28 @@ Shader "Custom/LinearTrailShaderTopToBottom"
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
+            #pragma multi_compile_instancing
             #include "UnityCG.cginc"
 
             struct appdata_t
             {
                 float4 vertex : POSITION;
                 float2 uv : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
             };
 
             struct v2f
             {
                 float4 pos : SV_POSITION;
                 float2 uv : TEXCOORD0;
+                UNITY_VERTEX_OUTPUT_STEREO
             };
 
             float4 _Color;
 
             v2f vert (appdata_t v)
             {
+                UNITY_SETUP_INSTANCE_ID(v);
                 v2f o;
                 o.pos = UnityObjectToClipPos(v.vertex);
                 o.uv = v.uv;
--- Trench Game (V2)/Assets/scripts/ProjectileRenderer.cs
+++ Trench Game (V2)/Assets/scripts/ProjectileRenderer.cs
@@ -42,59 +42,35 @@ public class ProjectileRenderer : MonoBehaviour
 
     public void RenderBullets ()
     {
-        //var bulletManager = ProjectileManager.Manager;
         bulletMaterial.enableInstancing = true;
-        //var motionBlurSamples = bulletManager.motionBlurSamples;
-        //var ogColor = material.color;
 
         var bullets = ProjectileManager.Manager.activeBullets;
-        //Matrix4x4[] transforms = new Matrix4x4[bullets.Count];
+        Matrix4x4[] transforms = new Matrix4x4[bullets.Count];
 
         bulletMaterial.color = bulletColor;
 
         for (int i = 0; i < bullets.Count; i++)
         {
-
-
             var bullet = bullets[i];
-
-            //for (int l = 0; l < motionBlurSamples; l++)
-            //{
-            //float t = (float)l / (motionBlurSamples - 1);
-            //Vector3 samplePosition = bullet.pos - bullet.velocity * t * Time.deltaTime;
             var startToPos = bullet.pos - bullet.startPos;
             var clampedStartToPos = Vector2.ClampMagnitude(startToPos, bullet.range);
-            //var maxLength = Mathf.Min(maxLengthPerMPS * bullet.velocity.magnitude, startToPos.magnitude);
-            //var lengthVector = Vector2.ClampMagnitude(clampedStartToPos, maxLength);
-            //var lengthVector = bullet.velocity.normalized * Mathf.Min(startToPos.magnitude, bullet.velocity.magnitude);
-            //var clampedPos = clampedStartToPos + bullet.startPos;
             var maxLength = startToPos.magnitude * (1-(trailEndSpeed / bullet.velocity.magnitude));
             //var end = bullet.pos - lengthVector;
             var clampedPos = clampedStartToPos + bullet.startPos;
             var totalDist = startToPos.magnitude;
             var startToEnd = Mathf.Clamp(totalDist - maxLength, 0, bullet.range);
             var end = bullet.startPos + startToPos.normalized * startToEnd;
-            //var clampedPos = clampedStartToPos + bullet.startPos;
             var length = Vector2.Distance(clampedPos, end);
             var center = (end + clampedPos) /2;
-            //GeoFuncs.MarkPoint(end, 1, Color.white); GeoFuncs.MarkPoint(center,1, Color.white); GeoFuncs.MarkPoint(bullet.pos, 1, Color.white);
-            //var posToCenter = center - end;
             Vector3 scale = new Vector2(headSize,length); // Adjust as needed
             var rot = Quaternion.LookRotation(Vector3.forward, bullet.velocity);
             Matrix4x4 matrix = Matrix4x4.TRS(center, rot, scale);
-            //transforms[i] = matrix;
+            transforms[i] = matrix;
 
-            Graphics.DrawMesh(bulletMesh, matrix, bulletMaterial, 0);
+            //Graphics.DrawMesh(bulletMesh, matrix, bulletMaterial, 0);
         }
 
-        //foreach (var bullet in BulletManager.Manager.activeBullets)
-        //{
-        //    var rot = Quaternion.LookRotation(Vector3.forward, bullet.velocity);
-        //    Matrix4x4 matrix = Matrix4x4.TRS(bullet.pos, rot, Vector3.one * BulletManager.Manager.meshScale);
-        //    Graphics.DrawMesh(BulletManager.Manager.bulletMesh, matrix, bulletMaterial, 0);
-        //}
-
-        //Graphics.DrawMeshInstanced(bulletMesh, 0, bulletMaterial, transforms);
+        Graphics.DrawMeshInstanced(bulletMesh, 0, bulletMaterial, transforms);
     }
 
     public void RenderTrenches ()
